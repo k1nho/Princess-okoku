03f5e1355de1312fd8de9e145b64989c9b0ee748
k1nho <kinhong99@gmail.com>
2023-05-04 15:53:57 -0400

    adding color to playfield

--- src/Components/PlayField.tsx
+++ src/Components/PlayField.tsx
@@ -9,7 +9,7 @@ export const PlayField: React.FC<props> = ({ isEnemyField }) => {
     const invocation = isEnemyField ? "einvocation" : "invocation";
     const spell = isEnemyField ? "espell" : "spell";
     return (
-        <div className="flex flex-col justify-center items-center space-y-5">
+        <div className="flex flex-col justify-center items-center space-y-5 bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 p-7 rounded-lg">
             <div id="invocation-zone" className="flex gap-10">
                 <Card key={`${invocation}-1`}></Card>
                 <Card key={`${invocation}-2`}></Card>
@@ -22,7 +22,6 @@ export const PlayField: React.FC<props> = ({ isEnemyField }) => {
                 <Card key={`${spell}-3`}></Card>
                 <Card key={`${spell}-4`}></Card>
             </div>
-
         </div>
 
     );
