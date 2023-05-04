dd2eb8abe6909dfefa2e289adc67e9aee6d3f8b9
k1nho <kinhong99@gmail.com>
2023-05-04 15:54:38 -0400

    board

--- src/Components/Board.tsx
+++ src/Components/Board.tsx
@@ -1,13 +1,12 @@
 import { CardControls } from "./CardControls"
 import { PlayField } from "./PlayField"
-import ice_moon from "../assets/bgs/stage_ice_moon.png"
-import machina_world from "../assets/bgs/state_machina_world.png"
 import space_vortex from "../assets/bgs/state_space_vortex.png"
+import cosmos from "../assets/bgs/stage_cosmos.png"
 
 export const Board: React.FC = () => {
     return (
-        <div className="flex-col relative h-screen bg-cover bg-center" style={{ backgroundImage: `url(${space_vortex})` }}>
-            <div className="absolute top-4 left-4 p-4">
+        <div className="flex-col relative h-screen bg-cover bg-center" style={{ backgroundImage: `url(${cosmos})` }}>
+            <div className="absolute top-0 left-4 p-4">
                 <PlayField isEnemyField={true}></PlayField>
             </div>
             <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
@@ -15,7 +14,7 @@ export const Board: React.FC = () => {
                     Turn
                 </div>
             </div>
-            <div className="absolute bottom-4 right-4 p-4 flex">
+            <div className="absolute bottom-0 right-4 p-4 flex">
                 <PlayField isEnemyField={false}></PlayField>
             </div>
             <div className="absolute left-10 bottom-10 p-4">
