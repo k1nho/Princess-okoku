a7d994e32d4a0c4ddda9a9226709ce62e02ca18d
k1nho <kinhong99@gmail.com>
2023-05-04 15:54:26 -0400

    Quit button in battle

--- src/Components/CardControls.tsx
+++ src/Components/CardControls.tsx
@@ -1,12 +1,17 @@
+import usePlayerStore from "../store/store"
 import { Card } from "./Card"
 
 export const CardControls: React.FC = () => {
+    const [setGameMode] = usePlayerStore((state) => [state.setGameMode])
     return (
         <div className="flex gap-8">
             <Card></Card>
             <Card></Card>
             <Card></Card>
             <Card></Card>
+            <div className="flex items-center justify-center">
+                <button className="rounded-full px-2 py-0.5 bg-red-500 text-white" onClick={() => setGameMode("Dashboard")}>X</button>
+            </div>
         </div>
     )
 }
