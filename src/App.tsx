dc791f7ef7af6397499400a87b732a1094b55c28
k1nho <kinhong99@gmail.com>
2023-05-03 22:52:42 -0400

    remove

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Board } from "./Components/Board";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <div className="flex flex-col items-center justify-center">
+                <h1>Princess Okoku</h1>
+                <div className="flex space-x-2">
+                    <button className="px-2 py-2 rounded-full bg-pink-400 hover:bg-pink-500 text-gray-100">Start</button>
+                    <button className="px-2 py-2 rounded-full bg-amber-400 hover:bg-amber-500 text-gray-100">Info</button>
+                </div>
+            </div>
+            <Board />
+        </>
+    );
 }
 
-export default App
+export default App;
