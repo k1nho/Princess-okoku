747031bba130d39bd45323fb1d5f09cb4a40f830
k1nho <kinhong99@gmail.com>
2023-05-03 23:27:17 -0400

    change component based on mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import { Board } from "./Components/Board";
+import { MainMenu } from "./Components/MainMenu";
 
 function App() {
     return (
         <>
-            <div className="flex flex-col items-center justify-center">
-                <h1>Princess Okoku</h1>
-                <div className="flex space-x-2">
-                    <button className="px-2 py-2 rounded-full bg-pink-400 hover:bg-pink-500 text-gray-100">Start</button>
-                    <button className="px-2 py-2 rounded-full bg-amber-400 hover:bg-amber-500 text-gray-100">Info</button>
-                </div>
+            <div>
+                <Board></Board>
             </div>
-            <Board />
         </>
     );
 }
