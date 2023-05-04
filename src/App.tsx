5edc266b757f1f2576dfd432cb47cda429918ff8
k1nho <kinhong99@gmail.com>
2023-05-04 15:55:12 -0400

    menu based on gamemode

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import { Board } from "./Components/Board";
+import { Dashboard } from "./Components/Dashboard";
 import { MainMenu } from "./Components/MainMenu";
+import usePlayerStore from "./store/store";
 
 function App() {
+    const [gameMode] = usePlayerStore((state) => [state.gameMode])
+    if (gameMode === "MainMenu") {
+        return <MainMenu />
+    }
+
+    else if (gameMode === "Dashboard") {
+        return <Dashboard />
+    }
+
     return (
         <>
             <div>
