3380ddc15861a8fa3b48995f3feebaf9bc2332f7
k1nho <kinhong99@gmail.com>
2023-05-04 22:23:02 -0400

    adding to the cardpool

--- src/store/cardpool.ts
+++ src/store/cardpool.ts
@@ -8,7 +8,7 @@ export const cardpool: Card[] = [
     { id: "5", name: "Techno King", iml: "src/assets/decks/techno_princess/card_techno_king.png", atk: 5, def: 5, cost: 5, special: "instanthp 2" },
     { id: "6", name: "Techno Servant", iml: "src/assets/decks/techno_princess/card_techno_servant.png", atk: 3, def: 2, cost: 3, special: "" },
     { id: "7", name: "TCP Princess", iml: "src/assets/decks/techno_princess/card_princess_tcp.png", atk: 1, def: 2, cost: 1, special: "search 4" },
-    { id: "8", name: "", iml: "src/assets/decks/techno_princess/card_mini_princess.png", atk: 1, def: 2, cost: 1, special: "search 4" },
+    { id: "8", name: "Techno Princess", iml: "src/assets/decks/techno_princess/card_techno_princess.png", atk: 2, def: 3, cost: 3, special: "" },
     { id: "9", name: "Princess Router", iml: "src/assets/decks/techno_princess/qp_princess_router.png", atk: -1, def: -1, cost: 2, special: "instanthp 3" },
     { id: "10", name: "Princess Baton", iml: "src/assets/decks/techno_princess/qp_princess_baton.png", atk: -1, def: -1, cost: 3, special: "reducecost 1 5" },
     { id: "11", name: "Princess PC", iml: "src/assets/decks/techno_princess/qp_princess_computer.png", atk: -1, def: -1, cost: 1, special: "search 4" },
@@ -16,6 +16,7 @@ export const cardpool: Card[] = [
 
 
     { id: "13", name: "Barrier Dragon", iml: "src/assets/decks/dragon_empire/card_barrier_dragon.png", atk: 1, def: 3, cost: 2, special: "instanthp 2" },
-    { id: "14", name: "Dragon Pot", iml: "src/assets/decks/dragon_empire/card_dragon_pot.png", atk: 2, def: 1, cost: 1, special: "search 13" },
-    { id: "15", name: "Dragon Genie", iml: "src/assets/decks/dragon_empire/card_dragon_genie.png", atk: 2, def: 2, cost: 1, special: "search 16" },
+    { id: "14", name: "Dragon Pot", iml: "src/assets/decks/dragon_empire/card_dragon_pot.png", atk: 2, def: 1, cost: 1, special: "instantdmg 1" },
+    { id: "15", name: "Dragon Genie", iml: "src/assets/decks/dragon_empire/card_dragon_genie.png", atk: 2, def: 2, cost: 1, special: "search 13" },
+    { id: "16", name: "Golden Dragon", iml: "src/assets/decks/dragon_empire/card_dragon_genie.png", atk: 3, def: 2, cost: 2, special: "instantdmg 1" },
 ]
