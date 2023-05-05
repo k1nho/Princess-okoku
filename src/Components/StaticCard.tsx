a6cf16d0a76f96a663c49367ea58f6e04893bc2c
k1nho <kinhong99@gmail.com>
2023-05-05 17:06:41 -0400

    static card changes

--- src/Components/StaticCard.tsx
+++ src/Components/StaticCard.tsx
@@ -11,15 +11,16 @@ export const StaticCard: React.FC<props> = ({ id }) => {
 
         <div className="w-28">
             <div id="info" className="flex justify-center bg-yellow-500 relative text-white rounded-t-md">
-                <div className="font-semibold">{card.name}</div>
+                <div className="absolute -top-4 -left-6 rounded-full  px-3 py-1 border-2 border-stone-900 bg-blue-800 text-amber-100 font-semibold">{card.cost}</div>
+                <div className="font-semibold text-center">{card.name}</div>
             </div>
             <img src={card.iml} alt={card.name} className="w-28 h-28 rounded-sm" />
             <div className="flex justify-between text-amber-100 font-semibold bg-stone-900">
                 <div className="bg-red-700 rounded-br px-3 py-1 flex items-center justify-center">
-                    {card.atk}
+                    {card.atk > 0 ? card.atk : "S"}
                 </div>
                 <div className="bg-blue-700 rounded-bl px-3 py-1 flex items-center justify-center">
-                    {card.def}
+                    {card.def > 0 ? card.def : "S"}
                 </div>
             </div>
             <div className="flex flex-wrap bg-stone-900 rounded-b-md">
