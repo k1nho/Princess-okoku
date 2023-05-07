778e71989296edc2d14354369418c4ed4b4c733f
k1nho <kinhong99@gmail.com>
2023-05-07 01:03:27 -0400

    separate playing fields

--- src/Components/Board.tsx
+++ src/Components/Board.tsx
@@ -1,12 +1,25 @@
 import { CardControls } from "./CardControls"
 import { PlayField } from "./PlayField"
 import cosmos from "../assets/bgs/stage_cosmos.png"
+import { EplayField } from "./EPlayField"
+import usePlayerStore from "../store/store"
 
 export const Board: React.FC = () => {
+
+    const [checkWinCond] = usePlayerStore((state) => [state.battleWinCond])
+
+    if (checkWinCond) {
+        return (
+            <div>
+                victory
+            </div>
+        )
+    }
+
     return (
         <div className="flex-col relative h-screen bg-cover bg-center" style={{ backgroundImage: `url(${cosmos})` }}>
             <div className="absolute top-0 left-4 p-4">
-                <PlayField isEnemyField={true}></PlayField>
+                <EplayField />
             </div>
             <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
                 <div className=" text-4xl text-bold p-4 text-white rounded-xl bg-gradient-to-r from-purple-400 to-pink-600 ">
@@ -14,7 +27,7 @@ export const Board: React.FC = () => {
                 </div>
             </div>
             <div className="absolute bottom-0 right-4 p-4 flex">
-                <PlayField isEnemyField={false}></PlayField>
+                <PlayField />
             </div>
             <div className="absolute left-10 bottom-10 p-4">
                 <CardControls />
