cc204359042d6bf06080c187785582e73a0174cc
k1nho <kinhong99@gmail.com>
2023-05-07 01:04:38 -0400

    add key

--- src/Components/DeckDisplay.tsx
+++ src/Components/DeckDisplay.tsx
@@ -8,7 +8,7 @@ export const DeckDisplay: React.FC = () => {
         <div className="grid grid-cols-4 justify-items-center gap-4 bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-900 to-indigo-500 rounded-lg p-8 cursor-pointer">
             {deck.map((card) => (
                 <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }} key={card.id}>
-                    <StaticCard id={card.id} />
+                    <StaticCard key={card.id} id={card.id} />
                 </motion.div>
             ))}
 
