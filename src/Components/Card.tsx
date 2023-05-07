62f3f4034e76056399be8f0852e370a700ddbab0
k1nho <kinhong99@gmail.com>
2023-05-07 01:03:58 -0400

    convert card base it on gamephase

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,45 +1,57 @@
 import { useState } from "react";
-import aiprincess from "../assets/decks/techno_princess/card_ai_princess.png";
 import usePlayerStore, { getCard } from "../store/store";
 
 interface props {
-    id?: string
+    id?: string;
 }
 
 export const Card: React.FC<props> = ({ id }) => {
-    const [playedCards, addCardToPlay, removeCardFromHand] = usePlayerStore((state) => [state.battleInfo.playedCards, state.addCardToPlay, state.removeCardFromHand])
-    const [isCommandOpen, setIsCommandOpen] = useState(false)
-    const card = id ? getCard(id) : getCard("1")
+    const [playedCards, addCardToPlay, removeCardFromHand, gamePhase] = usePlayerStore(
+        (state) => [
+            state.battleInfo.playedCards,
+            state.addCardToPlay,
+            state.removeCardFromHand,
+            state.gamePhase
+        ]
+    );
+    const [isCommandOpen, setIsCommandOpen] = useState(false);
+    const card = id ? getCard(id) : getCard("1");
 
-    const handleCommandClose = (e: React.MouseEvent<HTMLDivElement, MouseEvent>, pos: number) => {
-        e.stopPropagation()
-        addCardToPlay(pos)
-        setIsCommandOpen(false)
-    }
-
+    const handleCommandClose = (
+        e: React.MouseEvent<HTMLDivElement, MouseEvent>,
+        pos: number
+    ) => {
+        e.stopPropagation();
+        removeCardFromHand(card.id);
+        addCardToPlay(pos, card);
+        setIsCommandOpen(false);
+    };
 
     return (
-        <div className="w-28 cursor-pointer" onClick={() => setIsCommandOpen(true)}>
+        <button className="w-28 cursor-pointer" onClick={() => setIsCommandOpen(true)} disabled={gamePhase !== "Attack"}>
             {isCommandOpen && (
-                <div
-                    className="fixed top-0 left-0 w-screen h-screen bg-gray-900 bg-opacity-50 flex justify-center items-center z-10"
-                >
+                <div className="fixed top-0 left-0 w-screen h-screen bg-gray-900 bg-opacity-50 flex justify-center items-center z-10">
                     <div className="p-2 rounded-md grid grid-cols-4 grid-rows-2 gap-2">
                         {playedCards.map((card, pos) => {
-                            return card ?
-                                <div key={pos} className="p-4 border-2 bg-stone-900 border-amber-300 rounded-md text-white font-semibold"
-                                    onClick={(e) => { e.stopPropagation(); setIsCommandOpen(false) }}>
-                                    X
-                                </div> :
-                                <div className="bg-red-500 p-4 border-2 border-amber-300 rounded-md text-white font-semibold" onClick={(e) => handleCommandClose(e, pos)}>
+                            return card ? null : (
+                                <div
+                                    className="bg-red-500 p-4 border-2 border-amber-300 rounded-md text-white font-semibold"
+                                    onClick={(e) => handleCommandClose(e, pos)}
+                                >
                                     {pos}
                                 </div>
+                            );
                         })}
                     </div>
                 </div>
             )}
-            <div id="info" className="flex justify-center bg-yellow-500 relative text-white rounded-t-md">
-                <div className="absolute -top-4 -left-6 rounded-full  px-3 py-1 border-2 border-stone-900 bg-blue-800 text-amber-100 font-semibold">{card.cost}</div>
+            <div
+                id="info"
+                className="flex justify-center bg-yellow-500 relative text-white rounded-t-md"
+            >
+                <div className="absolute -top-4 -left-6 rounded-full  px-3 py-1 border-2 border-stone-900 bg-blue-800 text-amber-100 font-semibold">
+                    {card.cost}
+                </div>
                 <div className="font-semibold">{card.name}</div>
             </div>
             <img src={card.iml} alt="ai_princess" className="w-28 h-28 rounded-sm" />
@@ -52,8 +64,15 @@ export const Card: React.FC<props> = ({ id }) => {
                 </div>
             </div>
             <div className="flex flex-wrap bg-stone-900 rounded-b-md">
-                {card.description ? <p className="w-full text-xs text-white p-2"><span className="text-red-500 font-bold">Effect: </span>{card.description}</p> : <p className="w-full text-xs text-white p-2">Normal card</p>}
+                {card.description ? (
+                    <p className="w-full text-xs text-white p-2">
+                        <span className="text-red-500 font-bold">Effect: </span>
+                        {card.description}
+                    </p>
+                ) : (
+                    <p className="w-full text-xs text-white p-2">Normal card</p>
+                )}
             </div>
-        </div>
-    )
-}
+        </button>
+    );
+};
