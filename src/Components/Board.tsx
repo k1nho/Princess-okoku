c686628d8517c250c806bcfbcdef328f5ef13b69
k1nho <kinhong99@gmail.com>
2023-05-07 14:45:08 -0400

    winconditions

--- src/Components/Board.tsx
+++ src/Components/Board.tsx
@@ -3,15 +3,29 @@ import { PlayField } from "./PlayField"
 import cosmos from "../assets/bgs/stage_cosmos.png"
 import { EplayField } from "./EPlayField"
 import usePlayerStore from "../store/store"
+import { GiCrown } from "react-icons/gi"
 
 export const Board: React.FC = () => {
 
-    const [checkWinCond] = usePlayerStore((state) => [state.battleWinCond])
+    const [checkWinCond, turn, setGameMode, finishBattle] = usePlayerStore((state) => [state.battleWinCond, state.turn, state.setGameMode, state.finishBattle])
+
+    const handleVictory = () => {
+        finishBattle()
+        setGameMode("Dashboard")
+    }
 
     if (checkWinCond) {
         return (
-            <div>
-                victory
+            <div className="flex flex-col justify-center items-center">
+                <div>
+                    <h1 className="font-bold text-4xl">Victory</h1>
+                </div>
+                <button className="flex items-center space-x-2 bg-pink-400 rounded-full px-4 py-2 hover:bg-pink-600 transition ease-in-out duration-700 text-white" onClick={handleVictory}>
+
+                    <GiCrown />
+                    <p>Dashboard</p>
+                </button>
+
             </div>
         )
     }
@@ -23,7 +37,7 @@ export const Board: React.FC = () => {
             </div>
             <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
                 <div className=" text-4xl text-bold p-4 text-white rounded-xl bg-gradient-to-r from-purple-400 to-pink-600 ">
-                    Turn
+                    Turn {turn}
                 </div>
             </div>
             <div className="absolute bottom-0 right-4 p-4 flex">
