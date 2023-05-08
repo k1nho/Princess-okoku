39d7ddd86bf6a2b4e5245cbcfde5d4c16c6f59fb
k1nho <kinhong99@gmail.com>
2023-05-07 23:32:43 -0400

    message

--- src/Components/Tutorials/ConfirmDeck.tsx
+++ src/Components/Tutorials/ConfirmDeck.tsx
@@ -4,7 +4,7 @@ export const ConfirmDeck = () => {
     return (
         <div className="flex flex-col justify-center space-y-4 items-center">
             <div className="font-semibold text-2xl text-stone-50">
-                All set you have selected this deck to start your adventure
+                All set, you have selected this deck to start your adventure
             </div>
             <div
             >
