69788f9f1bef752bcdedcc0607c413850df005f3
k1nho <kinhong99@gmail.com>
2023-05-08 21:41:36 -0400

    get correct display

--- src/Components/DeckDisplay.tsx
+++ src/Components/DeckDisplay.tsx
@@ -1,14 +1,16 @@
 import { motion } from "framer-motion"
-import usePlayerStore from "../store/store"
+import usePlayerStore, { getDeck } from "../store/store"
 import { StaticCard } from "./StaticCard"
 
 export const DeckDisplay: React.FC = () => {
-    const [deck] = usePlayerStore((state) => [state.deck])
+    const [deckId] = usePlayerStore((state) => [state.deck])
+    const deck = [...getDeck(deckId * 12, (deckId * 12) + 12).map((c) => ({ ...c }))]
+
     return (
         <div className="grid grid-cols-4 justify-items-center gap-4 bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-900 to-indigo-500 rounded-lg p-8 cursor-pointer">
             {deck.map((card) => (
                 <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }} key={card.id}>
-                    <StaticCard key={card.id} id={card.id} />
+                    <StaticCard id={card.id} />
                 </motion.div>
             ))}
 
