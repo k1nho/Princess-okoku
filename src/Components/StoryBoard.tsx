353db400077c69c483f3dde41ed452fb39ca1fe2
k1nho <kinhong99@gmail.com>
2023-05-09 18:08:14 -0400

    storyboard

--- src/Components/StoryBoard.tsx
+++ src/Components/StoryBoard.tsx
@@ -1,12 +1,11 @@
 import usePlayerStore from "../store/store"
-import { GiCrown } from "react-icons/gi"
 import { StoryTechno } from "./scenarios/StoryTechno";
 import { StoryFairy } from "./scenarios/StoryFairy";
 import { StoryDragon } from "./scenarios/StoryDragon";
 import { StoryCats } from "./scenarios/StoryCats";
 
 export const StoryBoard: React.FC = () => {
-    const [setGameMode, level] = usePlayerStore((state) => [state.setGameMode, state.level])
+    const [level] = usePlayerStore((state) => [state.level])
 
     const renderStory = () => {
         switch ((level) % 4) {
@@ -25,12 +24,8 @@ export const StoryBoard: React.FC = () => {
 
 
     return (
-        <div>
+        <div className="min-h-screen flex items-center justify-center">
             {renderStory()}
-            <button className="flex items-center space-x-2 bg-pink-400 rounded-full px-4 py-2 hover:bg-pink-600 transition ease-in-out duration-700 text-white" onClick={() => setGameMode("Dashboard")}>
-                <p>Dashboard</p>
-                <GiCrown />
-            </button>
         </div>
     )
 }
