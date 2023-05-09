2c89a929fd32a8e5306c48db85943f3e6944e4fe
k1nho <kinhong99@gmail.com>
2023-05-09 18:46:50 -0400

    story backgrounds

--- src/Components/StoryBoard.tsx
+++ src/Components/StoryBoard.tsx
@@ -4,8 +4,18 @@ import { StoryFairy } from "./scenarios/StoryFairy";
 import { StoryDragon } from "./scenarios/StoryDragon";
 import { StoryCats } from "./scenarios/StoryCats";
 
+const storybgs = [
+    "src/assets/bgs/stage_machina_world.png",
+    "src/assets/bgs/bg_story_dragon_px.png",
+    "src/assets/bgs/stage_road_to_starry.png",
+    "src/assets/bgs/bg_story_cats.png"
+];
+
 export const StoryBoard: React.FC = () => {
     const [level] = usePlayerStore((state) => [state.level])
+
+    const storybg = storybgs[level % 4]
+
 
     const renderStory = () => {
         switch ((level) % 4) {
@@ -24,7 +34,7 @@ export const StoryBoard: React.FC = () => {
 
 
     return (
-        <div className="min-h-screen flex items-center justify-center">
+        <div className="min-h-screen flex items-center justify-center bg-cover bg-center" style={{ backgroundImage: `url(${storybg})` }}>
             {renderStory()}
         </div>
     )
