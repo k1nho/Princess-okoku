30a25bf41c3642009b9346ef09f10684ee46b6ab
k1nho <kinhong99@gmail.com>
2023-05-08 21:43:56 -0400

    computer controls

--- src/Components/ComputerControls.tsx
+++ src/Components/ComputerControls.tsx
@@ -3,29 +3,43 @@ import usePlayerStore from "../store/store";
 import { CardPlaceholder } from "./CardPlaceholder";
 
 export const ComputerControls: React.FC = () => {
-    const [handCards, gamePhase, setGamePhase, setWinCond, prepareDraw, addCardToHand, removeCardFromHand, setTurn, resetEnergy] =
+    const [handCards, playedCards, userPlayedCards, gamePhase, setGamePhase, setWinCond, prepareDraw, addCardToHand, removeCardFromHand, addCardToEPlay, energy, setEnemyEnergy, attackCard, attackLp, setTurn, resetEnergy, getEPlayedCards, getUPlayedCards] =
         usePlayerStore((state) => [
             state.enemyBattleInfo.handCards,
+            state.enemyBattleInfo.playedCards,
+            state.battleInfo.playedCards,
             state.gamePhase,
             state.setGamePhase,
             state.setWinCond,
             state.prepareEDraw,
             state.addCardToEHand,
             state.removeECardFromHand,
+            state.addCardToEPlay,
+            state.enemyBattleInfo.energy,
+            state.setEnemyEnergy,
+            state.attackCard,
+            state.attacklp,
             state.setTurn,
-            state.resetEnergy
+            state.resetEnergy,
+            state.getEPlayedCards,
+            state.getPlayedCards
         ]);
 
     useEffect(() => {
-        const validCards = handCards.reduce((acc, val) => {
+        const validHandCards = handCards.reduce((acc, val) => {
             if (val !== null) return acc + 1;
             return acc;
         }, 0);
 
+        const validPlayCards = playedCards.reduce((acc, val) => {
+            if (val !== null) return acc + 1;
+            return acc
+        }, 0)
+
         const computerPlay = () => {
             // DRAW STEP
             const idxRemove = Math.floor(Math.random() * 5)
-            if (validCards === 5) {
+            if (validHandCards === 5) {
                 const selectedCard = handCards[idxRemove];
                 if (selectedCard !== null) removeCardFromHand(selectedCard.id);
             }
@@ -33,7 +47,41 @@ export const ComputerControls: React.FC = () => {
             addCardToHand()
 
             // INVOKE STEP
+            if (validPlayCards < 8) {
+                const idx = playedCards.indexOf(null)
+                if (idx !== -1) {
+                    for (let i = 0; i < handCards.length; i++) {
+                        const eCard = handCards[i];
+                        if (eCard !== null && energy - eCard.cost >= 0) {
+                            removeCardFromHand(eCard.id)
+                            addCardToEPlay(idx, eCard)
+                            setEnemyEnergy(eCard.cost)
+                        }
+                    }
+                }
+            }
+
             // ATTACK STEP
+            const pCards = getEPlayedCards();
+            console.log(pCards)
+            for (let i = 0; i < pCards.length; i++) {
+                const eCard = pCards[i];
+                if (eCard === null) continue;
+                const uCards = getUPlayedCards();
+                const uIdx = uCards.findIndex((card) => card !== null)
+                console.log("User field", uCards)
+                if (uIdx === -1) {
+
+                    console.log("lifepoint attack: ", eCard.atk)
+                    attackLp(eCard.atk)
+                }
+                else {
+                    const uCard = uCards[uIdx]
+                    console.log("attacking cards: ", uCard?.name)
+                    if (uCard !== null) attackCard(uCard, eCard)
+                }
+            }
+
             // END STEP
             resetEnergy()
             setTurn()
@@ -44,7 +92,7 @@ export const ComputerControls: React.FC = () => {
         if (gamePhase === "EDraw") {
             computerPlay()
         }
-    }, [gamePhase, handCards, addCardToHand, prepareDraw, removeCardFromHand, setGamePhase, setWinCond, resetEnergy, setTurn])
+    }, [gamePhase, handCards, addCardToHand, prepareDraw, removeCardFromHand, setGamePhase, setWinCond, resetEnergy, setTurn, addCardToEPlay, attackCard, attackLp, energy, playedCards, setEnemyEnergy, userPlayedCards, getEPlayedCards, getUPlayedCards])
 
     return (
         <div className="flex gap-8">
