c7574b2cf545abb1491bb618bbefc931e5076aa4
k1nho <kinhong99@gmail.com>
2023-05-09 14:26:58 -0400

    stories

---
+++ src/Components/scenarios/StoryDragon.tsx
@@ -0,0 +1,101 @@
+const dialogs = [
+    {
+        name: "Narrator",
+        dialog:
+            "As Princess Kaede wanders through the kingdom, she comes across a group of dragons huddled together in fear. She approaches them cautiously, unsure of what to expect.",
+    },
+
+    { name: "Princess Kaede", dialog: " AHHHHHHHH Dragon!!!" },
+    {
+        name: "Princess Kaede",
+        dialog: "So, you're saying that you're not dangerous?",
+    },
+    {
+        name: "Dad Dragon",
+        dialog:
+            "Please don't be afraid of us. We're not the dangerous creatures that people make us out to be.",
+    },
+    {
+        name: "Zig",
+        dialog: "That's right! We're actually pretty scared creatures ourselves.",
+    },
+    {
+        name: "Dragon Genie",
+        dialog:
+            "And we do what we can to avoid any conflict. That's why we live on this volcano.",
+    },
+    { name: "Princess Kaede", dialog: "Why do you live on a volcano?" },
+    {
+        name: "Dad Dragon",
+        dialog:
+            "It's an active volcano, which makes it difficult for people to reach us. We like our privacy.",
+    },
+    {
+        name: "Zig",
+        dialog: "And it's also very lonely up here. We don't get many visitors.",
+    },
+    {
+        name: "Dragon Genie",
+        dialog: "But we make the best of it. We have each other.",
+    },
+    {
+        name: "Princess Kaede",
+        dialog:
+            "I can understand that. Sometimes being alone is better than dealing with people.",
+    },
+    {
+        name: "Dad Dragon",
+        dialog: "That's true. But it's also important to have friends.",
+    },
+    {
+        name: "Zig",
+        dialog: "Friends are important. They make life more bearable.",
+    },
+    {
+        name: "Dragon Genie",
+        dialog: "That's right. And we're always here if you need a friend.",
+    },
+    {
+        name: "Princess Kaede",
+        dialog:
+            "I'm still trying to understand why the dragons are so afraid. They're such majestic creatures, and yet they seem so scared all the time. I wonder if there's more to them than what meets the eye.",
+    },
+    {
+        name: "Zig",
+        dialog:
+            "I just wish people would stop seeing my dad as a mindless beast. He's the kindest and most loving creature I know. He always makes sure I'm safe and happy, even if it means we have to live in a dangerous place like this volcano. I love him so much, and it hurts me when others fear and hate him without even knowing him.",
+    },
+    { name: "Dad Dragon", dialog: "Zig..." },
+    {
+        name: "Princess Kaede",
+        dialog:
+            "I see your dad is amazing Zig. Excuse me, Dragon Genie. I was wondering if you could tell me if there are any places in Althreisha where I might ",
+    },
+
+    {
+        name: "Princess Kaede",
+        dialog:
+            "Excuse me, Dragon Genie. I was wondering if you could help me find a clue about the world. You see, I'm lost and I'm trying to search for the truth behind this kingdom. Do you know of any place where I might find some answers?",
+    },
+
+    {
+        name: "Dragon Genie",
+        dialog:
+            "I've heard of the Starry Road, where fairies live. They're said to be the opposite of dragons - very kind and humble. Maybe you'll find some answers there, Princess Kaede.",
+    },
+    { name: "Princess Kaede", dialog: "Thank you. I appreciate that." },
+    { name: "Dad Dragon", dialog: "No problem. We're happy to help." },
+    { name: "Zig", dialog: "And if you ever need anything, just let us know." },
+    {
+        name: "Dragon Genie",
+        dialog: "We'll do what we can to make sure you're safe.",
+    },
+    {
+        name: "Narrator",
+        dialog: "As Princess Kaede nods goodbye, she realized that these creatures were not just fearsome beasts, but also had their own unique and awe-inspiring qualities. Despite their intimidating appearance, the dragons of Althreisha were truly remarkable creatures.",
+    },
+];
+
+export const StoryDragon = () => {
+    return <div></div>;
+};
