56358788b900f5a26fadcfbf50410ebb4b148fc3
k1nho <kinhong99@gmail.com>
2023-05-10 00:05:58 -0400

    cards import

--- src/Components/cardImages.ts
+++ src/Components/cardImages.ts
@@ -10,6 +10,42 @@ import pcard8 from "../assets/decks/techno_princess/qp_princess_router.png";
 import pcard9 from "../assets/decks/techno_princess/qp_princess_baston.png";
 import pcard10 from "../assets/decks/techno_princess/qp_princess_computer.png";
 import pcard11 from "../assets/decks/techno_princess/qp_princess_portal.png";
+import dcard0 from "../assets/decks/dragon_empire/card_barrier_dragon.png";
+import dcard1 from "../assets/decks/dragon_empire/card_dragon_pot.png";
+import dcard2 from "../assets/decks/dragon_empire/card_dragon_genie.png";
+import dcard3 from "../assets/decks/dragon_empire/card_golden_dragon.png";
+import dcard4 from "../assets/decks/dragon_empire/card_flash_dragon.png";
+import dcard5 from "../assets/decks/dragon_empire/card_zzz_dragon.png";
+import dcard6 from "../assets/decks/dragon_empire/card_dad_dragon.png";
+import dcard7 from "../assets/decks/dragon_empire/card_zig_dragon.png";
+import dcard8 from "../assets/decks/dragon_empire/qp_dragon_heal.png";
+import dcard9 from "../assets/decks/dragon_empire/qp_dragon_map.png";
+import dcard10 from "../assets/decks/dragon_empire/qp_dragon_power.png";
+import dcard11 from "../assets/decks/dragon_empire/qp_dragon_wand.png";
+import scard0 from "../assets/decks/spirit_fairies/card_baby_angel.png";
+import scard1 from "../assets/decks/spirit_fairies/card_astro_fairy.png";
+import scard2 from "../assets/decks/spirit_fairies/card_pinku_fairy.png";
+import scard3 from "../assets/decks/spirit_fairies/card_yai_fairy.png";
+import scard4 from "../assets/decks/spirit_fairies/card_yia_fairy.png";
+import scard5 from "../assets/decks/spirit_fairies/card_balance_fairy.png";
+import scard6 from "../assets/decks/spirit_fairies/card_ballet_fairy.png";
+import scard7 from "../assets/decks/spirit_fairies/card_health_fairy.png";
+import scard8 from "../assets/decks/spirit_fairies/qp_fairy_dress.png";
+import scard9 from "../assets/decks/spirit_fairies/qp_spirit_boost.png";
+import scard10 from "../assets/decks/spirit_fairies/qp_spirit_heal.png";
+import scard11 from "../assets/decks/spirit_fairies/qp_magic_wand.png";
+import ccard0 from "../assets/decks/talking_cats/card_berose.png";
+import ccard1 from "../assets/decks/talking_cats/card_koma_doll.png";
+import ccard2 from "../assets/decks/talking_cats/card_kurochi.png";
+import ccard3 from "../assets/decks/talking_cats/card_mitten.png";
+import ccard4 from "../assets/decks/talking_cats/card_nichirinko.png";
+import ccard5 from "../assets/decks/talking_cats/card_risto.png";
+import ccard6 from "../assets/decks/talking_cats/card_stringer.png";
+import ccard7 from "../assets/decks/talking_cats/card_troas.png";
+import ccard8 from "../assets/decks/talking_cats/qp_funbox.png";
+import ccard9 from "../assets/decks/talking_cats/qp_pasristo.png";
+import ccard10 from "../assets/decks/talking_cats/qp_heltroas.png";
+import ccard11 from "../assets/decks/talking_cats/qp_shredder.png";
 
 export const cardImages = [
     pcard0,
@@ -24,4 +60,40 @@ export const cardImages = [
     pcard9,
     pcard10,
     pcard11,
+    dcard0,
+    dcard1,
+    dcard2,
+    dcard3,
+    dcard4,
+    dcard5,
+    dcard6,
+    dcard7,
+    dcard8,
+    dcard9,
+    dcard10,
+    dcard11,
+    scard0,
+    scard1,
+    scard2,
+    scard3,
+    scard4,
+    scard5,
+    scard6,
+    scard7,
+    scard8,
+    scard9,
+    scard10,
+    scard11,
+    ccard0,
+    ccard1,
+    ccard2,
+    ccard3,
+    ccard4,
+    ccard5,
+    ccard6,
+    ccard7,
+    ccard8,
+    ccard9,
+    ccard10,
+    ccard11
 ];
