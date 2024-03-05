867152060cb01740b05d9f470e43fdf268a162c7
k1nho <kinhong99@gmail.com>
2024-03-05 13:51:23 -0500

    fix: highlight selected deck in tutorial

--- src/Components/Tutorials/ChooseDeck.tsx
+++ src/Components/Tutorials/ChooseDeck.tsx
@@ -1,174 +1,194 @@
 import { motion } from "framer-motion";
 import { StaticCard } from "../StaticCard";
 import usePlayerStore from "../../store/store";
+import { useState } from "react";
 
 export const ChooseDeck: React.FC = () => {
-    const [setDeck] = usePlayerStore((state) => [state.setDeck]);
+  const [setDeck] = usePlayerStore((state) => [state.setDeck]);
+  const [selectedDeckId, setSelectedDeckId] = useState(0);
 
-    const handleSetDeck = (id: number) => {
-        localStorage.setItem("po_playerdeck", id.toString());
-        setDeck(id);
-    };
+  const handleSetDeck = (id: number) => {
+    localStorage.setItem("po_playerdeck", id.toString());
+    setSelectedDeckId(id);
+    setDeck(id);
+  };
 
-    return (
-        <div className="flex flex-col">
-            <div className="flex justify-center font-semibold text-2xl p-2">
-                <h1 className="font-bold text-4xl text-white">
-                    Choose a starting deck
-                </h1>
-            </div>
-            <div className="flex  flex-wrap justify-evenly p-2">
-                <div
-                    id="techno-princess"
-                    className="w-1/2 bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-900 to-indigo-500 rounded-lg p-1 cursor-pointer"
-                    onClick={() => handleSetDeck(0)}
-                >
-                    <div className="flex justify-center font-bold text-2xl mb-4 text-stone-50">
-                        <h1>Techno Princess</h1>
-                    </div>
-                    <div className="flex justify-center space-x-10">
-                        <motion.div
-                            transition={{ duration: 0.5 }}
-                            whileHover={{ scale: 1.1 }}
-                        >
-                            <StaticCard id="4" />
-                        </motion.div>
-                        <motion.div
-                            transition={{ duration: 0.5 }}
-                            whileHover={{ scale: 1.1 }}
-                        >
-                            <StaticCard id="1" />
-                        </motion.div>
-                        <motion.div
-                            transition={{ duration: 0.5 }}
-                            whileHover={{ scale: 1.1 }}
-                        >
-                            <StaticCard id="7" />
-                        </motion.div>
-                    </div>
-                    <div className="flex flex-wrap font-semibold text-lg text-gray-100 text-center">
-                        <p>
-                            The Techno Princess deck is a tactical deck with emphasis on its
-                            key ruler the AI Princess, hack your way through Althreisha with
-                            this wonderful cyberpunk deck
-                        </p>
-                    </div>
-                </div>
-                <div
-                    id="dragon-empire"
-                    className=" w-1/2 bg-gradient-to-r from-orange-600 to-orange-500 rounded-lg p-1 cursor-pointer"
-                    onClick={() => handleSetDeck(1)}
-                >
-                    <div className="flex justify-center font-bold text-2xl mb-4 text-stone-50">
-                        <h1>Dragon Empire</h1>
-                    </div>
-                    <div className="flex justify-center space-x-10">
-                        <motion.div
-                            transition={{ duration: 0.5 }}
-                            whileHover={{ scale: 1.1 }}
-                        >
-                            <StaticCard id="14" />
-                        </motion.div>
-                        <motion.div
-                            transition={{ duration: 0.5 }}
-                            whileHover={{ scale: 1.1 }}
-                        >
-                            <StaticCard id="20" />
-                        </motion.div>
-                        <motion.div
-                            transition={{ duration: 0.5 }}
-                            whileHover={{ scale: 1.1 }}
-                        >
-                            <StaticCard id="13" />
-                        </motion.div>
-                    </div>
-                    <div className="flex flex-wrap font-semibold text-lg text-gray-100 text-center">
-                        <p>
-                            The Dragon Empire deck is a scorching deck with emphasis on the
-                            instant damage mechanic, set forth these legion of dragons as you
-                            explore through Althreisha
-                        </p>
-                    </div>
-                </div>
-
-                <div
-                    id="spirit-fairies"
-                    className="w-1/2 bg-[conic-gradient(at_bottom_left,_var(--tw-gradient-stops))] from-fuchsia-300 via-green-400 to-rose-700 rounded-lg p-4 cursor-pointer"
-                    onClick={() => handleSetDeck(2)}
-                >
-                    <div className="flex justify-center font-bold text-2xl mb-4 text-stone-50">
-                        <h1>Spirit Fairies</h1>
-                    </div>
-                    <div className="flex justify-center space-x-10">
-                        <motion.div
-                            transition={{ duration: 0.5 }}
-                            whileHover={{ scale: 1.1 }}
-                        >
-                            <StaticCard id="28" />
-                        </motion.div>
-
-                        <motion.div
-                            transition={{ duration: 0.5 }}
-                            whileHover={{ scale: 1.1 }}
-                        >
-                            <StaticCard id="32" />
-                        </motion.div>
-                        <motion.div
-                            transition={{ duration: 0.5 }}
-                            whileHover={{ scale: 1.1 }}
-                        >
-                            <StaticCard id="29" />
-                        </motion.div>
-                    </div>
-                    <div className="flex flex-wrap font-semibold text-lg text-gray-100 text-center">
-                        <p>
-                            The Spirit Fairies deck is a control deck with emphasis on the
-                            instant attack boost mechanic,use the fairies as your guiding
-                            light through Althreisha
-                        </p>
-                    </div>
-                </div>
-                <div
-                    id="dolls"
-                    className="w-1/2 bg-gradient-to-r from-sky-400 to-blue-500 rounded-lg p-4 cursor-pointer"
-                    onClick={() => handleSetDeck(3)}
-                >
-                    <div className="flex justify-center font-bold text-2xl mb-4 text-stone-50">
-                        <h1>Talking Cats</h1>
-                    </div>
-                    <div className="flex justify-center space-x-10">
-                        <motion.div
-                            transition={{ duration: 0.5 }}
-                            whileHover={{ scale: 1.1 }}
-                        >
-
-                            <StaticCard id="39" />
-                        </motion.div>
-
-                        <motion.div
-                            transition={{ duration: 0.5 }}
-                            whileHover={{ scale: 1.1 }}
-                        >
-
-                            <StaticCard id="44" />
-                        </motion.div>
-                        <motion.div
-                            transition={{ duration: 0.5 }}
-                            whileHover={{ scale: 1.1 }}
-                        >
-
-                            <StaticCard id="42" />
-                        </motion.div>
-                    </div>
-                    <div className="flex flex-wrap font-semibold text-lg text-gray-100 text-center">
-                        <p>
-                            The Talking Cats deck is a fast play deck with emphasis on the
-                            reduce cost mechanic, command these felines  through the lands
-                            of Althreisha
-                        </p>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="flex flex-col">
+      <div className="flex justify-center font-semibold text-2xl p-2">
+        <h1 className="font-bold text-4xl text-white">
+          Choose a starting deck
+        </h1>
+      </div>
+      <div className="flex  flex-wrap justify-evenly p-2">
+        <div
+          id="techno-princess"
+          className="w-1/2 bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-900 to-indigo-500 rounded-lg p-1 cursor-pointer"
+          style={{
+            border:
+              selectedDeckId === 0
+                ? "5px solid #FFD700"
+                : "5px solid transparent",
+          }}
+          onClick={() => handleSetDeck(0)}
+        >
+          <div className="flex justify-center font-bold text-2xl mb-4 text-stone-50">
+            <h1>Techno Princess</h1>
+          </div>
+          <div className="flex justify-center space-x-10">
+            <motion.div
+              transition={{ duration: 0.5 }}
+              whileHover={{ scale: 1.1 }}
+            >
+              <StaticCard id="4" />
+            </motion.div>
+            <motion.div
+              transition={{ duration: 0.5 }}
+              whileHover={{ scale: 1.1 }}
+            >
+              <StaticCard id="1" />
+            </motion.div>
+            <motion.div
+              transition={{ duration: 0.5 }}
+              whileHover={{ scale: 1.1 }}
+            >
+              <StaticCard id="7" />
+            </motion.div>
+          </div>
+          <div className="flex flex-wrap font-semibold text-lg text-gray-100 text-center">
+            <p>
+              The Techno Princess deck is a tactical deck with emphasis on its
+              key ruler the AI Princess, hack your way through Althreisha with
+              this wonderful cyberpunk deck
+            </p>
+          </div>
         </div>
-    );
+        <div
+          id="dragon-empire"
+          className=" w-1/2 bg-gradient-to-r from-orange-600 to-orange-500 rounded-lg p-1 cursor-pointer"
+          style={{
+            border:
+              selectedDeckId === 1 ? "5px solid #FFD700" : "5px solid #ea580c ",
+          }}
+          onClick={() => handleSetDeck(1)}
+        >
+          <div className="flex justify-center font-bold text-2xl mb-4 text-stone-50">
+            <h1>Dragon Empire</h1>
+          </div>
+          <div className="flex justify-center space-x-10">
+            <motion.div
+              transition={{ duration: 0.5 }}
+              whileHover={{ scale: 1.1 }}
+            >
+              <StaticCard id="14" />
+            </motion.div>
+            <motion.div
+              transition={{ duration: 0.5 }}
+              whileHover={{ scale: 1.1 }}
+            >
+              <StaticCard id="20" />
+            </motion.div>
+            <motion.div
+              transition={{ duration: 0.5 }}
+              whileHover={{ scale: 1.1 }}
+            >
+              <StaticCard id="13" />
+            </motion.div>
+          </div>
+          <div className="flex flex-wrap font-semibold text-lg text-gray-100 text-center">
+            <p>
+              The Dragon Empire deck is a scorching deck with emphasis on the
+              instant damage mechanic, set forth these legion of dragons as you
+              explore through Althreisha
+            </p>
+          </div>
+        </div>
+
+        <div
+          id="spirit-fairies"
+          className="w-1/2 bg-[conic-gradient(at_bottom_left,_var(--tw-gradient-stops))] from-fuchsia-300 via-green-400 to-rose-700 rounded-lg p-4 cursor-pointer"
+          style={{
+            border:
+              selectedDeckId === 2
+                ? "5px solid #FFD700"
+                : "5px solid transparent",
+          }}
+          onClick={() => handleSetDeck(2)}
+        >
+          <div className="flex justify-center font-bold text-2xl mb-4 text-stone-50">
+            <h1>Spirit Fairies</h1>
+          </div>
+          <div className="flex justify-center space-x-10">
+            <motion.div
+              transition={{ duration: 0.5 }}
+              whileHover={{ scale: 1.1 }}
+            >
+              <StaticCard id="28" />
+            </motion.div>
+
+            <motion.div
+              transition={{ duration: 0.5 }}
+              whileHover={{ scale: 1.1 }}
+            >
+              <StaticCard id="32" />
+            </motion.div>
+            <motion.div
+              transition={{ duration: 0.5 }}
+              whileHover={{ scale: 1.1 }}
+            >
+              <StaticCard id="29" />
+            </motion.div>
+          </div>
+          <div className="flex flex-wrap font-semibold text-lg text-gray-100 text-center">
+            <p>
+              The Spirit Fairies deck is a control deck with emphasis on the
+              instant attack boost mechanic,use the fairies as your guiding
+              light through Althreisha
+            </p>
+          </div>
+        </div>
+        <div
+          id="dolls"
+          className="w-1/2 bg-gradient-to-r from-sky-400 to-blue-500 rounded-lg p-4 cursor-pointer"
+          style={{
+            border:
+              selectedDeckId === 3 ? "5px solid #FFD700" : "5px solid #38bdf8",
+          }}
+          onClick={() => handleSetDeck(3)}
+        >
+          <div className="flex justify-center font-bold text-2xl mb-4 text-stone-50">
+            <h1>Talking Cats</h1>
+          </div>
+          <div className="flex justify-center space-x-10">
+            <motion.div
+              transition={{ duration: 0.5 }}
+              whileHover={{ scale: 1.1 }}
+            >
+              <StaticCard id="39" />
+            </motion.div>
+
+            <motion.div
+              transition={{ duration: 0.5 }}
+              whileHover={{ scale: 1.1 }}
+            >
+              <StaticCard id="44" />
+            </motion.div>
+            <motion.div
+              transition={{ duration: 0.5 }}
+              whileHover={{ scale: 1.1 }}
+            >
+              <StaticCard id="42" />
+            </motion.div>
+          </div>
+          <div className="flex flex-wrap font-semibold text-lg text-gray-100 text-center">
+            <p>
+              The Talking Cats deck is a fast play deck with emphasis on the
+              reduce cost mechanic, command these felines through the lands of
+              Althreisha
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
